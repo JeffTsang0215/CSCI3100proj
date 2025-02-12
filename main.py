9e44fe154c90fcfd5dcc29eb6fee84b57cfdd4e3
SinDyr <mowangmw@gmail.com>
2025-02-13 01:16:52 +0800

    add turn base

--- main.py
+++ main.py
@@ -41,6 +41,9 @@ def rotate(surface, angle, pivot, offset):
     rect = rotated_image.get_rect(center=pivot+rotated_offset)
     return rotated_image, rect  # Return the rotated image and shifted rect.
 
+def click_circle(mouse_pos, center, radius):
+    return(mouse_pos[0] - center[0])**2 + (mouse_pos[1] - center[1])**2 <= radius**2
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 fps = 60
@@ -66,6 +69,7 @@ class Card:
         self.atk = atk
         self.cost = cost
         self.ext = ext
+        self.attacked = False
         self.image = image.convert_alpha()
         self.image = pygame.transform.smoothscale(self.image, cardDim)
         
@@ -114,8 +118,25 @@ class Sys:
         for i in reversed(temp):
             self.aiCard.pop(i)
 
+    def switchTurn(self):
+        if (self.isPlayerTurn):
+            self.isPlayerTurn = False
+        else:
+            self.isPlayerTurn = True
+        for card in sys.myCard:
+            card.attacked = False
+        for card in sys.aiCard:
+            card.attacked = False
 
     def draw(self):
+        #middle line
+
+        #box indicate turn
+        if self.isPlayerTurn:
+            pygame.draw.lines(screen, (0, 0, 255), True, [(0, HEIGHT/2),(WIDTH, HEIGHT/2), (WIDTH, HEIGHT), (0, HEIGHT)], 5)
+        else:
+            pygame.draw.lines(screen, (255, 0, 0), True, [(0, 0),(WIDTH, 0), (WIDTH, HEIGHT/2), (0, HEIGHT/2)], 5)
+
         #card graphic me
         left = WIDTH/2 - (len(self.myCard)*(cardDim[0] + WIDTH/80) - WIDTH/80)/2
         top = HEIGHT*0.55
@@ -149,7 +170,6 @@ class Sys:
             else:
                 angle += 9
 
-        #pygame.draw.circle(screen, (255, 0, 0), [WIDTH*0.9, HEIGHT*0.9], 10)
 
 
         #card graphic ai
@@ -176,13 +196,21 @@ class Sys:
         pygame.draw.circle(screen, (242, 89, 0), [WIDTH/2+HEIGHT*0.05, HEIGHT*0.1+HEIGHT*0.05], HEIGHT*0.025)
         text(screen, str(self.aihp), (0, 0, 0), int(WIDTH/64), [WIDTH/2+HEIGHT*0.05, HEIGHT*0.1+HEIGHT*0.05], "center")
 
+        # draw arrow
         if (sys.clickedCard != -1 and sys.clickTimer > 0):
             pointA = (sys.myCard[sys.clickedCard].rect[0]+cardDim[0]/2, sys.myCard[sys.clickedCard].rect[1]+cardDim[1]/2)
             pointB = pygame.mouse.get_pos()
             angle_going = math.atan((pointB[1]-pointA[1])/(pointB[0]-pointA[0]+1e-10))
             if(pointB[0] < pointA[0]):
                 angle_going += math.pi
-            pygame.draw.polygon(screen, (255, 0, 0), [pointA, pointB, (pointB[0]+WIDTH/100*math.cos(angle_going-math.pi+math.pi/6), pointB[1]+WIDTH/100*math.sin(angle_going-math.pi+math.pi/6)), (pointB[0]+WIDTH/100*math.cos(angle_going-math.pi-math.pi/6), pointB[1]+WIDTH/100*math.sin(angle_going-math.pi-math.pi/6)), pointB])
+            pygame.draw.polygon(screen, (255, 0, 0), [pointA, pointB, (pointB[0]+WIDTH/100*math.cos(angle_going-math.pi+math.pi/6), pointB[1]+WIDTH/100*math.sin(angle_going-math.pi+math.pi/6)), (pointB[0]+WIDTH/100*math.cos(angle_going-math.pi-math.pi/6), pointB[1]+WIDTH/100*math.sin(angle_going-math.pi-math.pi/6)), pointB], 5)
+        
+        # end turn button
+        pygame.draw.circle(screen, (0, 0, 0), [WIDTH*0.95, HEIGHT/2], WIDTH/24)
+        pygame.draw.rect(screen, (0, 0, 0), [WIDTH*0.94, HEIGHT/2-WIDTH/24, 2*WIDTH/24, 2*WIDTH/24])
+        pygame.draw.circle(screen, (255, 255, 255), [WIDTH*0.95, HEIGHT/2], WIDTH/25)
+        pygame.draw.rect(screen, (255, 255, 255), [WIDTH*0.95, HEIGHT/2-WIDTH/25, 2*WIDTH/25, 2*WIDTH/25])
+        text(screen, "End Turn", (0, 0, 0), int(WIDTH/55), [WIDTH*0.96, HEIGHT/2], "center")
         
 
 
@@ -201,24 +229,33 @@ while running:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             sys.clickTimer += 1
+            
             #clicking my card
             sys.clickedCard = -1
-            for i in range(len(sys.myCard)):
-                if sys.myCard[i].rect.collidepoint(mouse_pos):
-                    sys.clickedCard = i
-                    break
+            if(sys.isPlayerTurn):
+                for i in range(len(sys.myCard)):
+                    if sys.myCard[i].rect.collidepoint(mouse_pos) and sys.myCard[i].attacked == False:
+                        sys.clickedCard = i
+                        break
+            
+            # switchTurn
+            if click_circle(mouse_pos, [WIDTH*0.95, HEIGHT/2], WIDTH/24) or (WIDTH*0.94 <= mouse_pos[0] <= WIDTH*0.94+2*WIDTH/24  and HEIGHT/2-WIDTH/24 <= mouse_pos[1] <= HEIGHT/2-WIDTH/24+2*WIDTH/24):
+                sys.switchTurn()
             
  
         if event.type == pygame.MOUSEBUTTONUP:
             sys.releasedCard = -1
-            for i in range(len(sys.aiCard)):
-                if sys.aiCard[i].rect.collidepoint(mouse_pos):
-                    sys.releasedCard = i
-                    break
-            if (mouse_pos[0] - WIDTH/2)**2 + (mouse_pos[1]-HEIGHT*0.1)**2 <= (HEIGHT*0.05)**2:
+            if(sys.isPlayerTurn):
+                for i in range(len(sys.aiCard)):
+                    if sys.aiCard[i].rect.collidepoint(mouse_pos):
+                        sys.releasedCard = i
+                        break
+            if click_circle(mouse_pos, [WIDTH/2, HEIGHT*0.1], HEIGHT*0.05):
                 sys.releasedCard = 99
+            #attack
             if (sys.clickedCard != -1 and sys.releasedCard != -1):
                 sys.attack(sys.clickedCard, sys.releasedCard)
+                sys.myCard[sys.clickedCard].attacked = True
                 sys.checkAlive()
 
             sys.clickTimer = 0
