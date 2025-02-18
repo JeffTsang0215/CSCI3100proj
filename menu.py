db3b901fc8f47c144ee8b31438f5c42daa0f5bcf
eddyng34 <eddyng34@gmail.com>
2025-02-18 20:53:42 +0800

    Add text on buttons in menu.py

--- menu.py
+++ menu.py
@@ -7,11 +7,11 @@ menu_bg = pygame.transform.scale(menu_bg, (shared.WIDTH, shared.HEIGHT))
 button_color = (137,84,39)
 hover_color = (204,134,76)
 border_color = (255, 255, 0)  # Yellow border color
-border_thickness = 5  # Border thickness
+border_thickness = round(0.005 * shared.HEIGHT) # Border thickness
 buttons = [
     [(0.42*shared.WIDTH , 0.33*shared.HEIGHT), (0.44*shared.WIDTH, 0.28*shared.HEIGHT), (0.565*shared.WIDTH, 0.28*shared.HEIGHT), (0.58*shared.WIDTH, 0.33*shared.HEIGHT)],  # Button 1
-    [(0.41*shared.WIDTH, 0.36*shared.HEIGHT), (0.42*shared.WIDTH, 0.35*shared.HEIGHT), (0.58*shared.WIDTH, 0.35*shared.HEIGHT), (0.59*shared.WIDTH, 0.40*shared.HEIGHT)],  # Button 2
-    #[(200, 350), (400, 350), (370, 400), (230, 400)],  # Button 3
+    [(0.415*shared.WIDTH, 0.345*shared.HEIGHT), (0.40*shared.WIDTH, 0.40*shared.HEIGHT), (0.60*shared.WIDTH, 0.40*shared.HEIGHT),(0.585*shared.WIDTH, 0.345*shared.HEIGHT)],  # Button 2
+    [(0.40*shared.WIDTH, 0.42*shared.HEIGHT), (0.42*shared.WIDTH, 0.48*shared.HEIGHT), (0.58*shared.WIDTH, 0.48*shared.HEIGHT),(0.60*shared.WIDTH, 0.42*shared.HEIGHT)],  # Button 3
     #[(200, 450), (400, 450), (370, 500), (230, 500)],  # Button 4
 ]
 
@@ -27,4 +27,5 @@ def menu_main(mouse_pos, mouse_click):
                 pygame.draw.polygon(shared.screen, border_color, button, border_thickness)  # Click effect
                 print(f"Button {i+1} clicked!")
 
+    shared.text(shared.screen, "Play Game", (0, 0, 0), int(shared.WIDTH/64), (0.50*shared.WIDTH, 0.305*shared.HEIGHT), "center")
 
